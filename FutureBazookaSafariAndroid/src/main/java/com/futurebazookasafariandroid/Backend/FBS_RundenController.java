6bc3fdd16d89d3a3c796ea7cd96730b0a250b569
exzelem <marvinbraun6@gmail.com>
2016-03-05 14:14:14 +0100

    Pathfinding
    eventuell die Lösung zum A Stern Problem, noch ungetestet

--- FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/Backend/FBS_RundenController.java
+++ FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/Backend/FBS_RundenController.java
@@ -8,6 +8,7 @@ package com.futurebazookasafariandroid.Backend;
 import com.futurebazookasafariandroid.FBS_Interfaces.FBS_LevelInterface;
 import com.futurebazookasafariandroid.FBS_Interfaces.FBS_MapInterface;
 import com.futurebazookasafariandroid.FBS_Interfaces.FBS_MonsterInterface;
+import com.futurebazookasafariandroid.FBS_Interfaces.FBS_SpielerInterface;
 import com.futurebazookasafariandroid.FBS_Level.FBS_LevelOne;
 import java.util.ArrayList;
 
@@ -23,10 +24,11 @@ public class FBS_RundenController {
     private FBS_MapInterface map;
     
 
-    public FBS_RundenController(FBS_MapController control) {
+    public FBS_RundenController(FBS_MapController control, FBS_SpielerInterface spieler) {
         this.mapcon = control; 
         this.map = mapcon.getMap();
-        this.starteRunde(new FBS_LevelOne(map.getStartpunkt().getX(), map.getStartpunkt().getY()));
+        mapcon.setSpieler(spieler);
+        starteRunde(new FBS_LevelOne(map.getStartpunkt().getX(), map.getStartpunkt().getY()));
     }
     public void starteRunde(FBS_LevelInterface Runde) {
         FBS_LevelInterface Level = Runde;
