07bac6f60e6639228796e2c2ab2e75db758d12be
Nico-MC <niconoster@t-online.de>
2016-03-06 19:05:18 +0100

    MapEditor muss angepasst werden. Texturen werden angezeigt.

--- Future Bazooka Safari/src/MapEditor/Info.java
+++
@@ -1,9 +0,0 @@
-/*
-Klassen sind noch in Bearbeitung.
-Ich arbeite ein wenig mit opengl.
-Daher auch die Fehler, da der Import bei euch wahrscheinlich fehlt.
-Später kann ja dann alles abgeändert werden.
-Erstmal abwarten was am Ende rauskommt.
-
-LG Nico
-*/
