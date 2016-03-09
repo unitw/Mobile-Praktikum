037cacd13eb9ff97ce316dbc4e1bf6dce0133d13
unitw <tw@ens-fiti.de>
2016-03-09 18:43:43 +0100

    background

--- FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/FXMLLoginScreenController.java
+++ FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/FXMLLoginScreenController.java
@@ -17,17 +17,13 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class FXMLLoginScreenController {
-    
+
     @FXML
     private TextField fbs_username;
     @FXML
     private PasswordField fbs_passwort;
     @FXML
     private GridPane gridlogin;
-    
-    
-  
-    
 
     @FXML
     private void LoginPressed(ActionEvent event) throws IOException, SQLException {
@@ -38,6 +34,8 @@ public class FXMLLoginScreenController {
 
                 Parent root = FXMLLoader.load(getClass().getResource("FXMLMainMenu.fxml"));
 
+              
+
                 Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
                 stage.setScene(new Scene(root, primScreenBounds.getWidth(), primScreenBounds.getHeight()));
                 stage.setX(0);
@@ -45,20 +43,32 @@ public class FXMLLoginScreenController {
                 stage.show();
             }
         } catch (ClassNotFoundException | SQLException ex) {
-            
+
         }
 
     }
     
+    
+    
+    
+     @FXML
+    public void initialize() {   
+          String image = FXMLLoginScreenController.class.getResource("logo_nue.png").toExternalForm();
+                gridlogin.setStyle("-fx-background-image: url('" + image + "'); "
+                        + "-fx-background-position: center center; "
+                        + "-fx-background-repeat: stretch;");
+
+    }
+
     @FXML
     private void SignUpPressed(ActionEvent event) throws IOException {
-        
+
         try {
-            if(FutureBazookaSafariAndroid.datenbank.register(fbs_username.getText(), fbs_passwort.getText())){
+            if (FutureBazookaSafariAndroid.datenbank.register(fbs_username.getText(), fbs_passwort.getText())) {
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                
+
                 Parent root = FXMLLoader.load(getClass().getResource("FXMLMainMenu.fxml"));
-                
+
                 Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
                 stage.setScene(new Scene(root, primScreenBounds.getWidth(), primScreenBounds.getHeight()));
                 stage.setX(0);
@@ -67,7 +77,6 @@ public class FXMLLoginScreenController {
             }
         } catch (ClassNotFoundException | SQLException ex) {
         }
-        
-        
+
     }
 }
