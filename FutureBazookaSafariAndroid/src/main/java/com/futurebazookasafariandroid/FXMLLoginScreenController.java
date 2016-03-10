840a3f05e32f0eda0c6e5fb6e37b65965aac0a23
unitw <tw@ens-fiti.de>
2016-03-10 11:27:18 +0100

    Oberfläche fancy

--- FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/FXMLLoginScreenController.java
+++ FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/FXMLLoginScreenController.java
@@ -10,9 +10,15 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -24,6 +30,8 @@ public class FXMLLoginScreenController {
     private PasswordField fbs_passwort;
     @FXML
     private GridPane gridlogin;
+    @FXML
+    private Label titellogin;
 
     @FXML
     private void LoginPressed(ActionEvent event) throws IOException, SQLException {
@@ -33,8 +41,6 @@ public class FXMLLoginScreenController {
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
                 Parent root = FXMLLoader.load(getClass().getResource("FXMLMainMenu.fxml"));
-
-              
 
                 Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
                 stage.setScene(new Scene(root, primScreenBounds.getWidth(), primScreenBounds.getHeight()));
@@ -47,16 +53,55 @@ public class FXMLLoginScreenController {
         }
 
     }
-    
-    
-    
-    
-     @FXML
-    public void initialize() {   
-          String image = FXMLLoginScreenController.class.getResource("logo_nue.png").toExternalForm();
-                gridlogin.setStyle("-fx-background-image: url('" + image + "'); "
-                        + "-fx-background-position: center center; "
-                        + "-fx-background-repeat: stretch;");
+
+    @FXML
+    public void initialize() {
+        String image = FXMLLoginScreenController.class.getResource("logo_nue.png").toExternalForm();
+        gridlogin.setStyle("-fx-background-image: url('" + image + "'); "
+                + "-fx-background-position: center center; "
+                + "-fx-background-repeat: stretch;");
+
+        Blend blend = new Blend();
+        blend.setMode(BlendMode.MULTIPLY);
+
+        DropShadow ds = new DropShadow();
+        ds.setColor(Color.rgb(254, 235, 66, 0.3));
+        ds.setOffsetX(5);
+        ds.setOffsetY(5);
+        ds.setRadius(5);
+        ds.setSpread(0.2);
+
+        blend.setBottomInput(ds);
+
+        DropShadow ds1 = new DropShadow();
+        ds1.setColor(Color.web("#f13a00"));
+        ds1.setRadius(20);
+        ds1.setSpread(0.2);
+
+        Blend blend2 = new Blend();
+        blend2.setMode(BlendMode.MULTIPLY);
+
+        InnerShadow is = new InnerShadow();
+        is.setColor(Color.web("#feeb42"));
+        is.setRadius(9);
+        is.setChoke(0.8);
+        blend2.setBottomInput(is);
+
+        InnerShadow is1 = new InnerShadow();
+        is1.setColor(Color.web("#f13a00"));
+        is1.setRadius(5);
+        is1.setChoke(0.4);
+        blend2.setTopInput(is1);
+
+        Blend blend1 = new Blend();
+        blend1.setMode(BlendMode.MULTIPLY);
+        blend1.setBottomInput(ds1);
+        blend1.setTopInput(blend2);
+
+        blend.setTopInput(blend1);
+
+     
+        titellogin.setEffect(blend);
 
     }
 
--- FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/Frontend/ControllerSpieloberflaeche.java
+++ FutureBazookaSafariAndroid/src/main/java/com/futurebazookasafariandroid/Frontend/ControllerSpieloberflaeche.java
@@ -97,6 +97,10 @@ public class ControllerSpieloberflaeche {
 
     @FXML
     protected void initialize() {
+        
+        
+        
+        
 
         
     }
